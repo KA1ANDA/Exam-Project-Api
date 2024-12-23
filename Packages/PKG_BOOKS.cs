0b045bde24c40fa81f32042cebce75ed62c4b774
Nika <118761674+ChanTo6@users.noreply.github.com>
2024-12-23 19:34:42 +0400

    packacges allowed

---
+++ Packages/PKG_BOOKS.cs
@@ -0,0 +1,122 @@
+﻿using ExamProjectApi.Models;
+using Oracle.ManagedDataAccess.Client;
+using System.Numerics;
+
+namespace ExamProjectApi.Packages
+{
+
+    public interface IPKG_BOOKS
+    {
+        public void add_book(Book book);
+        public List<Book> get_books();
+        public void delete_book(Book book);
+        public void update_book(Book book);
+    }
+    public class PKG_BOOKS:PKG_BASE , IPKG_BOOKS
+    {
+        IConfiguration configuration;
+
+        public PKG_BOOKS(IConfiguration configuration) : base(configuration) { }
+
+        public void add_book(Book book)
+        {
+
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_books.add_book";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_name", OracleDbType.Varchar2).Value = book.Name;
+            cmd.Parameters.Add("v_author", OracleDbType.Varchar2).Value = book.Author;
+            cmd.Parameters.Add("v_pice", OracleDbType.Int32).Value = book.Price;
+            cmd.Parameters.Add("v_quantity", OracleDbType.Int32).Value = book.Quantity;
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+
+        public List<Book> get_books()
+        {
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_books.get_books";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_result", OracleDbType.RefCursor).Direction = System.Data.ParameterDirection.Output;
+
+            OracleDataReader reader = cmd.ExecuteReader();
+            List<Book> books = new List<Book>();
+
+            while (reader.Read())
+            {
+                Book book = new Book();
+                book.Id = int.Parse(reader["id"].ToString());
+                book.Name = reader["name"].ToString();
+                book.Author = reader["author"].ToString();
+                book.Price = int.Parse(reader["price"].ToString());
+                book.Quantity = int.Parse(reader["quantity"].ToString());
+
+                books.Add(book);
+            }
+            reader.Close();
+            conn.Close();
+            return books;
+
+        }
+
+        public void delete_book(Book book)
+        {
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_books.delete_book";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_id", OracleDbType.Int32).Value = book.Id;
+
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+
+
+        public void update_book(Book book)
+        {
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_books.update_book";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_id", OracleDbType.Int32).Value = book.Id;
+            cmd.Parameters.Add("v_name", OracleDbType.Varchar2).Value = book.Name;
+            cmd.Parameters.Add("v_author", OracleDbType.Varchar2).Value = book.Author;
+            cmd.Parameters.Add("v_pice", OracleDbType.Int32).Value = book.Price;
+
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+
+    }
+}
---
+++ Packages/PKG_ORDERS.cs
@@ -0,0 +1,128 @@
+﻿using ExamProjectApi.Models;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ExamProjectApi.Packages
+{
+
+    public interface IPKG_ORDERS
+    {
+        public void create_order(Order order);
+        public void add_book_to_order(OrderDetails order);
+        public List<CartOrder> get_user_cart_by_customer();
+        public void add_to_cart(CartOrder order);
+    }
+
+
+    public class PKG_ORDERS : PKG_BASE, IPKG_ORDERS
+    {
+        IConfiguration configuration;
+
+        public PKG_ORDERS(IConfiguration configuration) : base(configuration) { }
+
+        public void create_order(Order order)
+        {
+
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_book_order.create_order";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_customer_name", OracleDbType.Varchar2).Value = order.CustomerName;
+            cmd.Parameters.Add("v_total_amount", OracleDbType.Int32).Value = order.TotalAmount;
+           
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        public void add_book_to_order(OrderDetails order)
+        {
+
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_book_order.add_book_to_order";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_order_id", OracleDbType.Int32).Value = order.OrderId;
+            cmd.Parameters.Add("v_book_id", OracleDbType.Int32).Value = order.BookId;
+            cmd.Parameters.Add("v_quantity", OracleDbType.Int32).Value = order.Quantity;
+            cmd.Parameters.Add("v_total_price", OracleDbType.Int32).Value = order.TotalPrice;
+
+
+
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+
+        public List<CartOrder> get_user_cart_by_customer()
+        {
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_book_order.get_user_cart_by_customer";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_result", OracleDbType.RefCursor).Direction = System.Data.ParameterDirection.Output;
+
+            OracleDataReader reader = cmd.ExecuteReader();
+            List<CartOrder> orderDetails = new List<CartOrder>();
+
+            while (reader.Read())
+            {
+                CartOrder orderDetail = new CartOrder();
+                orderDetail.Id = int.Parse(reader["id"].ToString());
+                orderDetail.CustomerName = reader["customername"].ToString();
+                orderDetail.BookId = int.Parse(reader["bookid"].ToString());
+                orderDetail.Quantity = int.Parse(reader["quantity"].ToString());
+                orderDetail.TotalPrice = int.Parse(reader["totalprice"].ToString());
+
+                orderDetails.Add(orderDetail);
+            }
+            reader.Close();
+            conn.Close();
+            return orderDetails;
+
+        }
+
+        public void add_to_cart(CartOrder order)
+        {
+
+            OracleConnection conn = new OracleConnection();
+            conn.ConnectionString = Connstr;
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "olerning.pkg_tk_book_order.add_to_cart";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("v_customer_name", OracleDbType.Varchar2).Value = order.CustomerName;
+            cmd.Parameters.Add("v_book_id", OracleDbType.Int32).Value = order.BookId;
+            cmd.Parameters.Add("v_quantity", OracleDbType.Int32).Value = order.Quantity;
+            cmd.Parameters.Add("v_total_price", OracleDbType.Int32).Value = order.TotalPrice;
+
+
+
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+    }
+}
